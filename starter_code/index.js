299127398b23cdb77bd3bd02daa8a58c42503e10
valeygal1990 <valentinesakwa@Valentines-Air.home>
2016-04-18 09:27:03 -0400

    Initial commit
    This is my first commit of homework 5

---
+++ starter_code/index.js
@@ -0,0 +1,63 @@
+$(document).ready(function())
+
+
+//User enters a city name in the container 
+
+//event input city name
+
+//and hits the submit button.
+
+var cityName = ()
+
+$ ('#submit-btn').click (changePageBackground(){
+	$('input:text').val(cityName)
+}
+
+
+event input
+
+function checkCityName(){
+	//code to check the city name
+
+	var city = document.getElementByClass('')
+}
+
+
+function changePageBackground (){
+	city = $(.austin);
+	city = $(.la);
+	city = $(.nyc);
+	city = $(.sf);
+	city = $(.sydney);
+	city = $(.citipix_skyline)
+
+	if (city){
+		event.preventDefault();
+		$('.austin').html(Austin);
+		$('body').removeClass();
+		$('body').css('.austin');
+	}
+
+}
+
+
+
+
+
+//User clicks on submit or presses enter on their keyboard
+//The entire webpage refreshes
+//Background image changes to the image of the corresponding city.
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
